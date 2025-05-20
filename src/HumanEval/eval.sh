1984a12a681f047ad4a291417c00f20ae4365602
Trevor Poon <s2652867@sms.ed.ac.uk>
2025-05-20 20:28:05 +0100

    Update README to include Hugging Face CLI login step and comment out CUDA environment variables in eval.sh for clarity and flexibility.

--- src/HumanEval/eval.sh
+++ src/HumanEval/eval.sh
@@ -7,19 +7,19 @@
 #SBATCH --time=0-08:00:00
 #SBATCH --output=log/slurm-eval-%j.out   # %j = Job ID
 
-export CUDA_HOME=/opt/cuda-9.0.176.1/
+# export CUDA_HOME=/opt/cuda-9.0.176.1/
 
-export CUDNN_HOME=/opt/cuDNN-7.0/
+# export CUDNN_HOME=/opt/cuDNN-7.0/
 
 export STUDENT_ID=$(whoami)
 
-export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 
-export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+# export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
 
-export CPATH=${CUDNN_HOME}/include:$CPATH
+# export CPATH=${CUDNN_HOME}/include:$CPATH
 
-export PATH=${CUDA_HOME}/bin:${PATH}
+# export PATH=${CUDA_HOME}/bin:${PATH}
 
 export PYTHON_PATH=$PATH
 
